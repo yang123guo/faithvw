afdb75922430f8028fd529d8f96d040f90e6aca3
yangguoqiang01 <wochange@163.com>
2019-07-10 23:40:03 +0800

    first commit

---
+++ src/components/form/form.vue
@@ -0,0 +1,29 @@
+<template>
+    <form>
+        <slot>
+
+        </slot>
+    </form>
+</template>
+<script>
+export default {
+    name: 'faForm',
+    // 上面传递两个参数
+    props: {
+        model: { // 数据源
+            type: Object
+        },
+        rules: { // 校验规则
+            type: Object
+        }
+    },
+    methods: {
+
+    },
+}
+</script>
+<style lang="less">
+    @import "./index.less";
+</style>
+
+
