cf9088c74b65afb073d5b6f1e9378bcfa08b39e6
yangguoqiang01 <wochange@163.com>
2019-07-22 00:02:44 +0800

    go on

---
+++ src/components/tabs/tab.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="">
+        <div :class="getClass(item)"
+            v-for="(item, index) in tabList"
+            :key="item.name || index"
+            @click="handleChange(index)">
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            tabList: []
+        }
+    },
+    methods: {
+        getClass(item) {
+            return [
+                    'fa-tab',
+                    {
+                        'fa-tab-disabled': item.disabled,
+                        'fa-tab-active': item.name === this.activeKey
+                    }
+                ];
+        },
+        handleChange(index) {
+            const nav = this.tabList[index];
+             // 当时禁选 那么return
+            if (nav.disabled) return;
+
+            // 把当前nav（根据TabPane求得）的name作为当前选中
+            this.activeKey = nav.name;
+            this.$emit('input', nav.name);
+            this.$emit('on-click', nav.name);
+        }
+    },
+}
+</script>
