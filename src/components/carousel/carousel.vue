8a056d5428192c8ad6ea3ef8e0032debe350445c
yangguoqiang01 <wochange@163.com>
2019-07-27 23:39:43 +0800

    go on

--- src/components/carousel/carousel.vue
+++ src/components/carousel/carousel.vue
@@ -14,13 +14,14 @@
             <Icon type="ios-arrow-forward"></Icon>
         </button>
         <ul class="carousel-dots">
-            <template v-for="n in slides.length">
-                <li :class="[n - 1 === currentIndex ? prefixCls + '-active' : '']"
-                    @click="dotsEvent('click', n - 1)"
-                    @mouseover="dotsEvent('hover', n - 1)">
-                    <button type="button" :class="[radiusDot ? 'radius' : '']"></button>
-                </li>
-            </template>
+            <!-- 当前高亮条件： currentIndex 等于 此个item所在的index -->
+            <li :class="[n - 1 === currentIndex ? 'dot-active' : '']"
+                v-for="n in slides.length"
+                :key="n"
+                @click="dotsEvent('click', n - 1)"
+                @mouseover="dotsEvent('hover', n - 1)">
+                <button type="button" :class="[radiusDot ? 'radius' : '']"></button>
+            </li>
         </ul>
     </div>
 </template>
@@ -29,19 +30,69 @@
 export default {
     data() {
         return {
-
+            slides: []
         };
     },
     computed: {
 
     },
+    props: {
+        radiusDot: {
+            type: Boolean,
+            dafault: false
+        },
+        autoplay: {
+            type: Boolean,
+            default: false
+        },
+        autoplaySpeed: {
+            type: Number,
+            default: 2000
+        },
+        loop: {
+            type: Boolean,
+            default: false
+        },
+    },
     methods: {
+        // 发现子元素  通过他就可以知道 子元素的个数 也就是每个item的轮播图
+        findChild (cb) {
+            const find = function (child) {
+                const name = child.$options.componentName;
+
+                // 子组件的名字
+                if (name) {
+                    cb(child);
+                } else if (child.$children.length) {
+                    child.$children.forEach((innerChild) => {
+                        find(innerChild, cb);
+                    });
+                }
+            };
+
+            if (this.slideInstances.length || !this.$children) {
+                this.slideInstances.forEach((child) => {
+                    find(child);
+                });
+            } else {
+                this.$children.forEach((child) => {
+                    find(child);
+                });
+            }
+        },
         arrowEvent() {
+
+        },
+        updateSlides() {
 
         }
     },
-    created() {
-
+    mounted() {
+        // 更新轮播图
+        this.updateSlides(true);
+        this.handleResize();
+        // 自动播放
+        this.setAutoplay();
     },
     components: {},
     watch: {},
