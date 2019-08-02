e51e89a0ca3995fd195fa70259467764f73bfc84
yangguoqiang01 <wochange@163.com>
2019-08-02 23:55:00 +0800

    go on

---
+++ src/components/affix/affix.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class=""></div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+
+        };
+    },
+    computed: {
+
+    },
+    methods: {
+
+    },
+    created() {
+
+    },
+    components: {},
+    watch: {},
+}
+</script>
+<style lang='less'>
+    @import "./index.less";
+</style>
