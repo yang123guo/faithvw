727949cc61b1c82835f58e352900dc3c9ae29f5f
yangguoqiang01 <wochange@163.com>
2019-08-03 23:56:51 +0800

    go on

--- src/components/affix/affix.vue
+++ src/components/affix/affix.vue
@@ -1,5 +1,10 @@
 <template>
-    <div class=""></div>
+    <div class="com-affix-wrap">
+        <div ref="point" class="com-affix" :style="styles">
+            <slot></slot>
+        </div>
+        <div v-show="slot" :style="slotStyle"></div>
+    </div>
 </template>
 
 <script>
